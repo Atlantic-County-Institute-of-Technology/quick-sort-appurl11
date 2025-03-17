b05b5e404360415ef5eb64b2ac8dcaf0ff45f16d
Maddie <appurlapple@gmail.com>
2025-03-17 14:20:18 -0400

    not much
    sorry not muhc done i aws working on something else too

---
+++ src/src/Main.java
@@ -0,0 +1,15 @@
+
+/*  Madison Bloom
+    Date Created: 3/17/25
+    Last Updated: 3/17/25
+    Title: Quick Sort
+ */
+public class Main {
+    public static void main(String[] args) {
+        String[] sort = new String[50];
+        Random random = new random;
+        for (int i = 0; i < sort.length(); i++) {
+            sort[i] ==
+        }
+    }
+}
