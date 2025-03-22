ac0437274bba6669d89fcc3440c01b7bd96f3e6b
Maddie <appurlapple@gmail.com>
2025-03-22 15:35:29 -0400

    quick update

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,4 +1,5 @@
-
+import java.util.Random;
+import java.util.Arrays;
 /*  Madison Bloom
     Date Created: 3/17/25
     Last Updated: 3/17/25
@@ -7,9 +8,9 @@
 public class Main {
     public static void main(String[] args) {
         String[] sort = new String[50];
-        Random random = new random;
-        for (int i = 0; i < sort.length(); i++) {
-            sort[i] ==
+        Random rand = new Random();
+        for (int i = 0; i < sort.length; i++) {
+            sort[i] == 
         }
     }
 }
