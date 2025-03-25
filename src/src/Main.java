100e51b4b84149fee1a43ef61365c5c86ecf44a3
Maddie <appurlapple@gmail.com>
2025-03-24 21:17:53 -0400

    Quick Update

--- src/src/Main.java
+++ src/src/Main.java
@@ -22,7 +22,7 @@ public class Main {
             System.out.println("Invalid");
             System.exit(choice);
         }
-        // Asks the user how long they want the sort to be, then does it
+        // Asks the user how long they want the sort to be, then makes the sort array the length they chose
 
         int[] sort = new int[length];
         Random rand = new Random();
